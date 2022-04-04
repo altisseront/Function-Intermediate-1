2d669ba605b4e6a7a5b052f6b2b7b97d8196c6d7
altisseront <altisseront@gmail.com>
2022-04-04 01:14:17 -0400

    intial

---
+++ functions intermediate 1.py
@@ -0,0 +1,89 @@
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+def updateValues():
+    x[1][0] = 15
+    students[1]["last_name"] = 'Bryant'
+    sports_directory['soccer'][0] = 'Andres'
+    z[0]['y'] = 30
+    print(x)
+    print(students)
+    print(sports_directory)
+    print(z)
+updateValues()
+
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+def iterateDictionary(x):
+    for i in range(0,len(x)):
+        for j in range(0,len(x[i])):
+            print(list(x[i])[j] + " - " + list(x[i].values())[j])
+print(iterateDictionary(students))
+
+
+
+
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+#first_name - Michael, last_name - Jordan
+#first_name - John, last_name - Rosales
+#first_name - Mark, last_name - Guillen
+#first_name - KB, last_name - Tonel
+
+
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterateDictionary2(key_name, some_list):
+    for i in some_list:
+        print(i[key_name])
+iterateDictionary2('first_name', students)
+
+def printInfo(some_dict):
+    key = list(some_dict.keys())
+    values = list(some_dict.values())
+    for i in range(0, len(some_dict)):
+        print(len(values[i]), key[i].upper())
+        for j in range(0,len(values[i])):
+            print(values[i][j])
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+printInfo(dojo)
+# output:
+7 LOCATIONS
+San Jose
+Seattle
+Dallas
+Chicago
+Tulsa
+DC
+Burbank
+    
+8 INSTRUCTORS
+Michael
+Amy
+Eduardo
+Josh
+Graham
+Patrick
+Minh
+Devon
